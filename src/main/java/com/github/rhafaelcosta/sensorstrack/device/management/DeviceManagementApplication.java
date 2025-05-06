4a49659642bf7f7347a38a7dad44a2b4846a31e4
Rhafael Freitas da Costa <rhafael.costa@lactec.org.br>
2025-05-06 11:40:21 -0300

    Criação do projeto spring boot

---
+++ src/main/java/com/github/rhafaelcosta/sensorstrack/device/management/DeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.rhafaelcosta.sensorstrack.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagementApplication.class, args);
+	}
+
+}
